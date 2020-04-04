1eff04815fb2559efdd66b26de565af079e11793
Alexander Franks <afranks@gmail.com>
2020-04-03 17:15:29 -0700

    added tests

---
+++ hw/hw1/tests/q2a.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q2a',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> B.shape\n(4, 3)',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> np.allclose(B @ v, '
+                                               'np.array([7, 12, 12, 32]))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw1/tests/q4e.py
@@ -0,0 +1,24 @@
+test = {   'name': 'q4e',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'np.all(np.isclose(f(np.array([-3, '
+                                               '0, 4])), [9, 0, 16]))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'np.all(np.isclose(df0(np.array([-3, '
+                                               '0, 4])), [0, 0, 0]))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'np.all(np.isclose(df8(np.array([-3, '
+                                               '0, 4])), [-112, -64, 0]))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
