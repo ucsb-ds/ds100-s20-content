492eeaa9b849a53be50f0eed1eb0b32e592696c0
Alexander Franks <afranks@gmail.com>
2020-04-16 15:31:04 -0700

    fixed empty test

--- hw/hw2/tests/q9a.py
+++ hw/hw2/tests/q9a.py
@@ -1,7 +1,18 @@
 test = {   'hidden': False,
     'name': 'q9a',
     'points': 5,
-    'suites': [   {   'cases': [],
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'np.abs(high_sample_size_unbiased_percent_trump '
+                                               '- 0.829) <= 0.02\n'
+                                               'True',
+                                       'hidden': True,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'np.abs(high_sample_size_biased_percent_trump '
+                                               '- 0.445) <= 0.04\n'
+                                               'True',
+                                       'hidden': True,
+                                       'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
