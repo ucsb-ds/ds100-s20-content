264ab3d98670c0eb5e2c6a32223c9e3540c4f87c
Alexander Franks <afranks@gmail.com>
2020-04-16 15:15:19 -0700

    Update q8d.py

--- hw/hw2/tests/q8d.py
+++ hw/hw2/tests/q8d.py
@@ -2,7 +2,7 @@ test = {   'hidden': False,
     'name': 'q8d',
     'points': 5,
     'suites': [   {   'cases': [   {   'code': '>>> 0 < percent_trump_biased < '
-                                               '1\n',
+                                               '1\n'
                                                'True'
                                        'hidden': False,
                                        'locked': False}],
