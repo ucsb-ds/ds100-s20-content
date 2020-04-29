b82a42a4a5f3d6ecdbc7a27ad0f4bc5f4f39d0e6
dreamboyMeng <fmeng@umail.ucsb.edu>
2020-04-29 10:58:54 -0700

    add lab5

---
+++ labs/lab5/student/tests/q1a.py
@@ -0,0 +1,25 @@
+test = {   'hidden': False,
+    'name': 'q1a',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               "bball[['defender_bin']].shape\n"
+                                               '(109604, 1)',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               "bball[['defender_bin']].iloc[0,]\n"
+                                               'defender_bin    (5.0, 6.0]\n'
+                                               'Name: 0, dtype: category\n'
+                                               'Categories (10, '
+                                               'interval[float64]): '
+                                               '[(-0.00855, 1.001] < (1.001, '
+                                               '2.001] < (2.001, 3.001] < '
+                                               '(3.001, 4.001] ... (6.0, 7.0] '
+                                               '< (7.0, 8.0] < (8.0, 9.0] < '
+                                               '(9.0, 9.999]]',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ labs/lab5/student/tests/q1b.py
@@ -0,0 +1,33 @@
+test = {   'hidden': False,
+    'name': 'q1b',
+    'points': 3,
+    'suites': [   {   'cases': [   {   'code': '>>> bball_percentages.shape\n'
+                                               '(10, 2)',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> bball_percentages.iloc[0, '
+                                               '1]\n'
+                                               '0.5763404747430254',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> bball_percentages.iloc[:, '
+                                               '1]\n'
+                                               'defender_bin\n'
+                                               '(-0.00855, 1.001]    0.576340\n'
+                                               '(1.001, 2.001]       1.597681\n'
+                                               '(2.001, 3.001]       2.499728\n'
+                                               '(3.001, 4.001]       3.510588\n'
+                                               '(4.001, 5.0]         4.489668\n'
+                                               '(5.0, 6.0]           5.463931\n'
+                                               '(6.0, 7.0]           6.450637\n'
+                                               '(7.0, 8.0]           7.447430\n'
+                                               '(8.0, 9.0]           8.454398\n'
+                                               '(9.0, 9.999]         9.450448\n'
+                                               'Name: defender_distance, '
+                                               'dtype: float64',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
