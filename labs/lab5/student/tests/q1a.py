e08960eed62480dca003166e8a7dab4b51d4326f
ykharitonova <31087970+ykharitonova@users.noreply.github.com>
2020-05-01 18:19:51 -0700

    Update q1a.py
    Change the test

--- labs/lab5/student/tests/q1a.py
+++ labs/lab5/student/tests/q1a.py
@@ -7,16 +7,8 @@ test = {   'hidden': False,
                                        'hidden': False,
                                        'locked': False},
                                    {   'code': '>>> '
-                                               "bball[['defender_bin']].iloc[0,]\n"
-                                               'defender_bin    (5.0, 6.0]\n'
-                                               'Name: 0, dtype: category\n'
-                                               'Categories (10, '
-                                               'interval[float64]): '
-                                               '[(-0.00855, 1.001] < (1.001, '
-                                               '2.001] < (2.001, 3.001] < '
-                                               '(3.001, 4.001] ... (6.0, 7.0] '
-                                               '< (7.0, 8.0] < (8.0, 9.0] < '
-                                               '(9.0, 9.999]]',
+                                               "bball[['defender_bin']].iloc[0,]['defender_bin'] == bball[['defender_bin']].iloc[4,]['defender_bin']\n"
+                                               'True',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
