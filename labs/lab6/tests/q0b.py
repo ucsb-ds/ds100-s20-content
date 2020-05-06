93e1b5b4840a0f94f377bc149df2e96d186dd030
BLimmie <brian01.lim@gmail.com>
2020-05-06 02:34:45 -0700

    lab6

---
+++ labs/lab6/tests/q0b.py
@@ -0,0 +1,19 @@
+test = {   'hidden': False,
+    'name': 'q0b',
+    'points': 4,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'sum(diabetes_df["target_above_mean"] '
+                                               '== True)\n'
+                                               '195',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'sum(diabetes_df["target_above_mean"] '
+                                               '== False)\n'
+                                               '247',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ labs/lab6/tests/q2b.py
@@ -0,0 +1,31 @@
+test = {   'hidden': False,
+    'name': 'q2b',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> diabetes_mean.size\n10',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'np.all(np.isclose(diabetes_mean, '
+                                               '[0]*10))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'np.all(np.isclose(np.zeros(10), '
+                                               'np.mean(normalized_features, '
+                                               'axis=0))) # make sure data is '
+                                               'centered at 0\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> -.003 < '
+                                               'np.sum(normalized_features[0]) '
+                                               '< 0.003 # make sure scaling '
+                                               'was done correctly\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
