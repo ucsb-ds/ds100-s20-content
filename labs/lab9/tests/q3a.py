5d1248175ad4d61fbc1cfff1331e7b79a2e6242a
BLimmie <brian01.lim@gmail.com>
2020-05-27 10:51:13 -0700

    updated lab9

--- labs/lab9/tests/q3a.py
+++ labs/lab9/tests/q3a.py
@@ -2,11 +2,13 @@ test = {   'hidden': False,
     'name': 'q3a',
     'points': 3,
     'suites': [   {   'cases': [   {   'code': '>>> 0.0275 < inf_deaths_r2 < '
-                                               '0.028\n',
+                                               '0.028\n'
+                                               'True',
                                        'hidden': False,
                                        'locked': False},
                                    {   'code': '>>> 0.0380 < child_deaths_r2 < '
-                                               '0.0385\n',
+                                               '0.0385\n'
+                                               'True',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
