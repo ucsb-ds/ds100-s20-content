b18be271ce704ac9fbb8614b8669fa3431e4813c
BLimmie <brian01.lim@gmail.com>
2020-05-27 12:34:01 -0700

    updated lab9

---
+++ labs/lab9/tests/q1a.py
@@ -0,0 +1,13 @@
+test = {   'hidden': False,
+    'name': 'q1a',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> len(trend_df)\n32',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> len(trend_df.columns)\n21',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
